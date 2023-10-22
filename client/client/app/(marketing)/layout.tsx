fa1296b9a5f01cc59440e45f3a1c97819c8dd160
Sky K <skyj.kim7@gmail.com>
2023-10-22 15:30:33 -0700

    added landing page/marketing page

---
+++ client/client/app/(marketing)/layout.tsx
@@ -0,0 +1,16 @@
+const MarketingLayout = ({
+    children
+}:{
+   children: React.ReactNode;
+}) => {
+    return(
+        <div className="h-full">
+            <Navbar />
+            <main className="h-full pt-40">
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default MarketingLayout;
