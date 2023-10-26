48f6de267b61b6297718518bc8a755ba6caf8fb0
Sky K <skyj.kim7@gmail.com>
2023-10-26 09:13:59 -0700

    reverted back the page.tsx

--- client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,36 +1,10 @@
-import { BlockNoteEditor } from "@blocknote/core";
-import{Editor} from "@/components/editor";
-import { useQuery, useMutation } from "convex/react";
-import { useMemo } from "react";
-import dynamic from "next/dynamic";
-
-
-import { api } from "@/convex/_generated/api";
-import { Id } from "@/convex/_generated/dataModel";
-
-
-interface DocumentIdPageProps{
-    params:{
-        documentId: Id<"documents">;
-    };
-};
-
-const DocumentIdPage = ({
-    params
-}: DocumentIdPageProps) => {
-    const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
-
-  const document = useQuery(api.documents.getById, {
-    documentId: params.documentId
-  });
-
-  const update = useMutation(api.documents.update);
-
-  const onChange = (content: string) => {
-    update({
-      id: params.documentId,
-      content
-    });
-  };
-
+const DocumentIdPage = () => {
+    return ( 
+        <div>
+            DocumentId
+        </div>
+     );
 }
+ 
+export default DocumentIdPage;
+
--- client/client/convex/documents.ts
+++ client/client/convex/documents.ts
@@ -3,6 +3,45 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+export const update = mutation({
+    args: {
+      id: v.id("documents"),
+      title: v.optional(v.string()),
+      content: v.optional(v.string()),
+      coverImage: v.optional(v.string()),
+      icon: v.optional(v.string()),
+      isPublished: v.optional(v.boolean())
+    },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      if (!identity) {
+        throw new Error("Unauthenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      const { id, ...rest } = args;
+  
+      const existingDocument = await ctx.db.get(args.id);
+  
+      if (!existingDocument) {
+        throw new Error("Not found");
+      }
+  
+      if (existingDocument.userId !== userId) {
+        throw new Error("Unauthorized");
+      }
+  
+      const document = await ctx.db.patch(args.id, {
+        ...rest,
+      });
+  
+      return document;
+    },
+  });
+  
+
 export const getById = query({
     args: { documentId: v.id("documents") },
     handler: async (ctx, args) => {
