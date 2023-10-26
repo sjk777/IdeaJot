c608ed61c53bc6f160c4f2a4ae614098c221f7bb
Sky K <skyj.kim7@gmail.com>
2023-10-26 11:53:53 -0700

    took off server files

--- client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -3,7 +3,7 @@
 import { useQuery, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import Toolbar from "@/components/toolbar";
+//import Toolbar from "@/components/toolbar";
 import { Editor } from "@/components/editor";
 
 interface DocumentIdPageProps{
@@ -35,9 +35,8 @@ const DocumentIdPage = ({
 
     return ( 
         <div className="pb-40">
-            <div className="h-[35vh]"/>
+            <div className="h-[15vh]"/>
             <div className="md:max-w-3xl lg:max-w-4xl mx-auo">
-                <Toolbar initialData={document} />
                 <Editor 
                 onChange={onChange}
                 initialContent={document.content}/>
