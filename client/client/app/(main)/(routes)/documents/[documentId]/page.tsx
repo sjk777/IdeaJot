899b2a469d44d43053f9a282bd183e5c4b658a8d
Sky K <skyj.kim7@gmail.com>
2023-10-26 08:49:37 -0700

    added getById

--- client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ client/client/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,9 @@
 import { BlockNoteEditor } from "@blocknote/core";
 import{Editor} from "@/components/editor";
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { useMemo } from "react";
+import dynamic from "next/dynamic";
+
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -13,3 +15,22 @@ interface DocumentIdPageProps{
     };
 };
 
+const DocumentIdPage = ({
+    params
+}: DocumentIdPageProps) => {
+    const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content
+    });
+  };
+
+}
--- client/client/convex/documents.ts
+++ client/client/convex/documents.ts
@@ -3,6 +3,35 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+export const getById = query({
+    args: { documentId: v.id("documents") },
+    handler: async (ctx, args) => {
+      const identity = await ctx.auth.getUserIdentity();
+  
+      const document = await ctx.db.get(args.documentId);
+  
+      if (!document) {
+        throw new Error("Not found");
+      }
+  
+      if (document.isPublished && !document.isArchived) {
+        return document;
+      }
+  
+      if (!identity) {
+        throw new Error("Not authenticated");
+      }
+  
+      const userId = identity.subject;
+  
+      if (document.userId !== userId) {
+        throw new Error("Unauthorized");
+      }
+  
+      return document;
+    }
+  });
+
 
 export const archive = mutation({
     args: {id: v.id("documents")},
@@ -48,7 +77,7 @@ export const archive = mutation({
         });
 
         recursiveArchive(args.id);
-        
+
         return document
     }
 })
